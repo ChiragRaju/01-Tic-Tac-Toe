a7290051372a987507601636696aa793f9d73b86
Chirag Raju <chiragrajus2002@gmail.com>
2024-04-21 15:53:47 +0530

    first commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import TicTacToe from "./components/tic-tac-toe";
+
+
+
+function App() {
+ 
+  return (
+    <div className="game">
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
